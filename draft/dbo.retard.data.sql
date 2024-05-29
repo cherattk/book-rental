40769dcbe8e63903d367fd9eb9e3af1bcda0a74e
Karim Cheratt <cherattk@users.noreply.github.com>
2024-05-29 15:33:05 -0400

    remove draft/ folder

--- draft/dbo.retard.data.sql
+++
@@ -1,6 +0,0 @@
-﻿insert into retard(pret, nbr_jour_retard, montant)
-select pretId, jourRetard, jourRetard*(10) as montant 
-from( 
-(select pret.id as pretId, DATEDIFF(day, pret.jour_retour_prevue, GETDATE()) as jourRetard 
-from pret where DATEDIFF(day, pret.jour_retour_prevue, GETDATE()) > 0)  ) as caculeRetard  
-where not exists(select pret from retard where retard.pret = 3);
